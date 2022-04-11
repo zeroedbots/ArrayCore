760188669518d806342b29e322b11d6a50b49297
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-04-11 08:20:24 +0530

    Added Porn Raid

--- ArrayCore/vc/vcraid.py
+++ ArrayCore/vc/vcraid.py
@@ -45,6 +45,16 @@ vid_list = [
     "./ArrayCore/Video/VID5.mp4",
 ]
 
+porn_list = [
+    "./ArrayCore/Porn/PORN1.mp4",
+    "./ArrayCore/Porn/PORN2.mp4",
+    "./ArrayCore/Porn/PORN3.mp4",
+    "./ArrayCore/Porn/PORN4.mp4",
+    "./ArrayCore/Porn/PORN5.mp4",
+    "./ArrayCore/Porn/PORN6.mp4",
+    "./ArrayCore/Porn/PORN7.mp4",
+]
+
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["raid"], prefixes=HNDLR))
 async def vcraid(_, e: Message):
     gid = e.chat.id
@@ -97,6 +107,52 @@ async def vcraid(_, e: Message):
     else:
          chat_id = gid
     vid = choice(vid_list)
+    if inp:
+        TheVenomXD = await e.reply_text("**Starting VC raid**")
+        link = f"https://itshellboy.tk/{vid[1:]}"
+        dlv = vid
+        songname = vid[18:]
+        if chat_id in QUEUE:
+            pos = add_to_queue(chat_id, songname, dlv, link, "Audio", 0)
+            await TheVenomXD.delete()
+            await e.reply_text(f"**> Raiding in:** {chat_.title} \n\n**> Audio:** {songname} \n**> Position:** #{pos}")
+        else:
+            if call_py1:
+                await call_py1.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py2:
+                await call_py2.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py3:
+                await call_py3.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py4:
+                await call_py4.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py5:
+                await call_py5.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py6:
+                await call_py6.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py7:
+                await call_py7.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py8:
+                await call_py8.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py9:
+                await call_py9.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            if call_py10:
+                await call_py10.join_group_call(chat_id, AudioVideoPiped(dlv), stream_type=StreamType().pulse_stream)
+            add_to_queue(chat_id, songname, dl, link, "Audio", 0)
+            await TheVenomXD.delete()
+            await e.reply_text(f"**> Raiding in:** {chat_.title} \n\n**> Audio:** {songname} \n**> Position:** Ongoing Raid")
+
+
+@vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["praid"], prefixes=HNDLR))
+async def vcraid(_, e: Message):
+    gid = e.chat.id
+    uid = e.from_user.id
+    if gid == uid:
+        inp = e.text[8:]
+        chat_ = await Venom1.get_chat(inp)
+        chat_id = chat_.id
+    else:
+         chat_id = gid
+    vid = choice(porn_list)
     if inp:
         TheVenomXD = await e.reply_text("**Starting VC raid**")
         link = f"https://itshellboy.tk/{vid[1:]}"
