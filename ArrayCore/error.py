09857d38a5e34c4992c68aac9b3c19985ac5bfa5
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-04-14 18:02:24 +0530

    Update error.py

--- ArrayCore/error.py
+++ ArrayCore/error.py
@@ -20,6 +20,13 @@ vcbot = Client(
     plugins={'root': 'ArrayCore.vc'},
 )
 
+def make_int(str_input):
+    str_list = str_input.split(" ")
+    int_list = []
+    for x in str_list:
+        int_list.append(int(x))
+    return int_list
+
 sudo = os.getenv("SUDO_USERS")
 SUDO_USERS = []
 if sudo:
