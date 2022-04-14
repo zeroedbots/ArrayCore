dd2aca1af93aa5bb6242860fc839a72d42b32e05
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-04-14 17:14:39 +0530

    Update restart.py

--- ArrayCore/vc/restart.py
+++ ArrayCore/vc/restart.py
@@ -4,7 +4,6 @@ import asyncio
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
 from time import time
 from datetime import datetime
 
