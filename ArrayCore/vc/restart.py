c3a15f9810d2b1b2222853e59ccde23a2e04816a
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-04-14 13:35:31 +0530

    Update restart.py

--- ArrayCore/vc/restart.py
+++ ArrayCore/vc/restart.py
@@ -12,3 +12,9 @@ async def ping(_, e: Message):
     await e.reply_text("`Restarting...`")
     os.execl(sys.executable, sys.executable, *sys.argv)
     quit()
+
+@vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["update"], prefixes=HNDLR))
+async def ping(_, e: Message):
+    await e.reply_text("__Updating...__")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
