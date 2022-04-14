e8dbaa9dcc741e2bd4b85d48bc3d4c5769522b6a
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-04-14 18:37:06 +0530

    Update restart.py

--- ArrayCore/vc/restart.py
+++ ArrayCore/vc/restart.py
@@ -4,8 +4,8 @@ import sys
 from pyrogram import filters
 from pyrogram.types import Message
 
-from .. import vcbot, HNDLR, SUDO_USERS
-    
+from . import vcbot, HNDLR, SUDO_USERS
+   
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=HNDLR))
 async def ping(_, e: Message):
