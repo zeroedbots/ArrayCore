6c06189ed1895c510d21078ccc696b5793d42017
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-04-14 18:08:00 +0530

    Update restart.py

--- ArrayCore/vc/restart.py
+++ ArrayCore/vc/restart.py
@@ -11,7 +11,8 @@ from ArrayCore.error import vcbot, HNDLR, SUDO_USERS
     
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=HNDLR))
 async def restart(_, e: Message):
-    await e.reply("`Restarting...`")
-    os.execl(sys.executable, sys.executable, *sys.argv)
+    if message.sender_chat in SUDO_USERS 
+        await e.reply("`Restarting...`")
+        os.execl(sys.executable, sys.executable, *sys.argv)
     # You probably don't need it but whatever
-    quit()
+        quit()
