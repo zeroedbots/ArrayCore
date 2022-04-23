58cb4648c3d8d1a3c3a30438437cb8be94125a7f
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-04-23 23:33:59 +0530

    Update start.py

--- ArrayCore/vc/start.py
+++ ArrayCore/vc/start.py
@@ -14,7 +14,7 @@ Array = "https://telegra.ph/file/fea7a0ef15a02dd5e4aac.jpg"
 Hn = "/"
 @vcbot.on_message(filters.private & filters.incoming & filters.command(['start'], prefixes=Hn))
 async def _start(_, ok: Message):
-        Array_msg = f"**Hello [{ok.from_user.first_name}](tg://user?id={ok.from_user.id}) !** \n\n __ • I'm ArrayCore An Advance And Simple Group Voice Call Bot__ \n\n **Click Below Buttons for More Info**"
+        Array_msg = f"**Hello [{ok.from_user.first_name}](tg://user?id={ok.from_user.id}) !** \n\n __ • I'm ArrayCore An Advance And Simple Group Voice Call Bot__ \n\n **Click Below Buttons for More Info** \n\n ** Note Use** `!vcraid` **Command For Now**"
         await ok.reply_photo(
         photo=Array,
         caption=Array_msg,
