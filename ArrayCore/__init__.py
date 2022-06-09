7de9fdea86d1343f71c2225b17f564ff7cc9b1f2
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-06-09 14:54:46 +0530

    Update __init__.py

--- ArrayCore/__init__.py
+++ ArrayCore/__init__.py
@@ -254,3 +254,68 @@ if SESSION25:
 else:
     Session25 = None
     call_py25 = None
+
+
+
+HELP_DICT = dict()
+
+HELP_DICT["Music Player"] = f"""
+**Basic music player commands!**
+
+**Command:** `!play`
+**Usage:** __Plays the song in voice chat. Supports replied audio, Youtube link or just a keyword to search.__
+**Example:** `!play Closer`
+
+**Command:** `!end`
+**Usage:** __Ends the music stream and leaves the voice chat.__
+
+**Command:** `!pause`
+**Usage:** __Pause the music stream in voice chat.__
+
+**Command:** `!list`
+**Usage:** __Shows the playlist in current chat.__
+
+**Command:** `!resume`
+**Usage:** __Resumes the paused stream in voice chat.__
+
+**Command:** `!skip`
+**Usage:** __Skips the current song playing in voice chat.__
+"""
+
+HELP_DICT["VC Raid"] = f"""
+**Voice Chat Raiding Commands!**
+
+**Command:** `!raid`
+**Usage:** __Raids the mentioned voice chat.__
+**Example:**
+    ~ `!vcraid chat username/id` [If in bot PM.]
+    ~ `!vcraid` [If in a group.]
+
+**Explanation:**
+    ▪︎First Join All Your Id's In The Group By !join `@chatusername` if chat is private Then !join `https://t.me/+rNTg-asHGZYzODY1` \n Then Do !raid In Groups 
+
+**Command:** `!raidend`
+**Usage:** __Stops the voice chat raid and leaves voice chat.__
+
+**Command:** `!raidpause`
+**Usage:** __Pauses the voice chat raid.__
+
+**Command:** `!raidresume`
+**Usage:** __Resumes the paused voice chat raid.__
+"""
+
+HELP_DICT["Extras"] = f"""
+**Some extra commands!**
+
+**Command:** `!help`
+**Usage:** __To see the help menu with all command details.__
+
+**Command:** `!start`
+**Usage:**  __To see the start message.__
+
+**Command:** `!join <username / invite link>`
+**Usage:** __Joins the chat with all clients.__
+
+**Command:** `!leave <username> / <chat-id>`
+**Usage:** __Leaves the chat with all clients.__
+"""
