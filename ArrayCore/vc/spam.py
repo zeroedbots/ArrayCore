e6a96052b7f8b1052940e983abf4c4073851e45d
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-06-09 16:29:08 +0530

    Update spam.py

--- ArrayCore/vc/spam.py
+++ ArrayCore/vc/spam.py
@@ -21,74 +21,59 @@ RiZ = ["@TheRiZoeL|@TheVenomXD|RiZoeL|Akash"]
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["spam"], prefixes=HNDLR))
 async def spam(_, e: Message):
-      gid = e.chat.id
-      uid = e.from_user.id
-      if gid == uid:
-          Grp = await vcbot.ask(chat_id=e.chat.id, text="**Send Group Link Or Username Where You want to spam**")
-          Group = Grp.text
-          if "/cancel" in Group:
-             return await e.reply_text("**All Process Cancelled**")
-          if Group.startswith("https://t.me/") or Group.startswith("@"):
-              if re.search(ChatS.lower(), Group.lower()):
-                 await e.reply_text("Sorry !! I can't Spam there")
-                 return
-              grpp = Group
-              try:
-                 await Session.join_chat(Group)
-              except Exception as ex:
-                 print(ex)
-              chat_ = await Session.get_chat(Group)
-              chat_id = chat_.id
-          else:
-              return await e.reply_text("**Send Group Link or Username**")
-          Ans = await vcbot.ask(chat_id=e.chat.id, text="**Send Counts**")
-          Num = Ans.text
-          if "/cancel" in Num:
-             return await e.reply_text("**All Process Cancelled**")
-          if Num.isnumeric():
-             count = int(Num)
-          else:
-             return await e.reply_text("Error! Send Counts in Numbers")
-          Ask = await vcbot.ask(chat_id=e.chat.id, text="**Send Spam Message To Spam**")
-          k = Ask.text
-          if "/cancel" in k:
-               return await e.reply_text("**All Process Cancelled**")
-          if re.search(RiZ.lower(), k.lower()):
+    usage = ""
+    if event.is_group:
+       Array = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+       if len(Array) == 2:
+           Chat_id = e.chat.id
+           Msgg = str(Array[1])
+           if re.search(RiZ.lower(), Msgg.lower()):
                return await e.reply_text("Sorry !! I can't Spam on @Arraycore's Owner")
-          Msgg = str(k)
-          ids = 0
-          try:
+           if int(Chat_id) in GRP:
+               return await e.reply_text("Sorry !! I Can't Spam here.")
+           counter = int(Array[0])
+           ids = 0
+           try:
               if Session:
                    ids += 1
+                   Session.join_chat(Chat_id)
                    for _ in range(count):
                       await Session.send_message(chat_id, Msgg)
                       await asyncio.sleep(0.3)
               if Session2:
                    ids += 1
-                   Session2.join_chat(Group)
+                   Session2.join_chat(Chat_id)
                    for _ in range(count):
                       await Session2.send_message(chat_id, Msgg)
                       await asyncio.sleep(0.3)
               if Session3:
                    ids += 1
-                   Session3.join_chat(Group)
+                   Session3.join_chat(Chat_id)
                    for _ in range(count):
                       await Session3.send_message(chat_id, Msgg)
                       await asyncio.sleep(0.3)
               if Session4:
                    ids += 1
-                   Session4.join_chat(Group)
+                   Session4.join_chat(Chat_id)
                    for _ in range(count):
                       await Session4.send_message(chat_id, Msgg)
                       await asyncio.sleep(0.3)
               if Session5:
                    ids += 1
-                   Session5.join_chat(Group)
+                   Session5.join_chat(Chat_id)
                    for _ in range(count):
                       await Session5.send_message(chat_id, Msgg)
                       await asyncio.sleep(0.3)
-              await vcbot.send_message(e.chat.id, f"**Started Spam** \n\n **Group:** `{Group}` \n **IDs:** `{ids}` \n **Spam Message:** `{Msgg}`")
+              await vcbot.send_message(e.chat.id, f"**Started Spam** \n\n **Group:** `{Chat_id}` \n **IDs:** `{ids}` \n **Spam Message:** `{Msgg}`")
 
-          except Exception as ex:
+           except Exception as ex:
                   await vcbot.send_message(e.chat.id, f"Error: `{ex}`")
                   print(ex)  
+           
+
+
+
+
+
+
+
